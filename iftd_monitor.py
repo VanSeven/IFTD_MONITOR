bdf1e11d97806de00471604b2ef1cebf2f00a2a2
VanSeven <1007977004@qq.com>
2019-05-16 23:18:14 +0800

    Add brake_monitor

--- iftd_monitor.py
+++ iftd_monitor.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 import array
 
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QCoreApplication, QTimer
 from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QSpacerItem,
                              QHBoxLayout, QSizePolicy, QPushButton, QApplication)
@@ -26,6 +26,7 @@ class IFTDMonitor(QMainWindow):
         self.plot_item_row = 2
         self.plot_item_col = 3
 
+        self.test_data = None
         self.data_list = []
         for i in range(self.plot_item_row * self.plot_item_col):
             self.data_list.append(array.array('d'))
@@ -46,8 +47,8 @@ class IFTDMonitor(QMainWindow):
         self.horizontalLayout.addItem(spacerItem)
         self.btn_start = QPushButton(self.centralwidget)
         self.horizontalLayout.addWidget(self.btn_start)
-        self.btn_stop = QPushButton(self.centralwidget)
-        self.horizontalLayout.addWidget(self.btn_stop)
+        self.btn_read_data = QPushButton(self.centralwidget)
+        self.horizontalLayout.addWidget(self.btn_read_data)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.setCentralWidget(self.centralwidget)
 
@@ -55,8 +56,13 @@ class IFTDMonitor(QMainWindow):
         self.custom_graphics_view_layout()
 
         for pi in self.plot_item_list:
-            self.curve_list.append(pi.plot(pen='y'))
+            self.curve_list.append(pi.plot(pen='g'))
         self.idx = 0
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.plot_data)
+        self.btn_start.clicked.connect(self.start_plot)
+        self.btn_read_data.clicked.connect(self.read_data)
 
     def custom_graphics_view_layout(self):
         self.main_graphics.setBackground('303030')
@@ -84,12 +90,15 @@ class IFTDMonitor(QMainWindow):
                 # 设置标注
                 pw.setLabel('left', text=y_label[i + j])
                 pw.setLabel('bottom', text='Time', units='s')
+                # 设置自适应坐标范围
+                pw.enableAutoRange(pg.ViewBox.YAxis, True)
                 self.plot_item_list.append(pw)
         self.main_graphics.setCentralWidget(gra_layout)
 
     def plot_data(self):
         for i, data in enumerate(self.data_list):
-            tmp = np.sin(np.pi / 50 * self.idx + 2 * np.pi * i / self.plot_item_col / self.plot_item_row)
+            # tmp = np.sin(np.pi / 50 * self.idx + 2 * np.pi * i / self.plot_item_col / self.plot_item_row)
+            tmp = self.test_data.iloc[self.idx, i]
             if len(data) < 200:
                 data.append(tmp)
             else:
@@ -101,20 +110,26 @@ class IFTDMonitor(QMainWindow):
 
         self.idx += 1
 
+    def read_data(self):
+        self.test_data = pd.read_csv('E:\\FTD\\FTPD-C919-10102-DY-190115-F-01-FTECAOWEN-ANA001-32.txt', sep='\s+',
+                         usecols=[1, 2, 3, 4, 5, 6], index_col=False, engine='c',
+                         skip_blank_lines=True)
+        print('Read Done.')
+
+    def start_plot(self):
+        self.timer.start(20)
+
     def translate_lang(self):
         _translate = QCoreApplication.translate
         self.setWindowTitle(_translate('MainWindow', 'IFTD MONITOR'))
         self.btn_start.setText(_translate('MainWindow', 'Start'))
-        self.btn_stop.setText(_translate('MainWindow', 'Stop'))
+        self.btn_read_data.setText(_translate('MainWindow', 'Read Data'))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mw = IFTDMonitor()
     mw.show()
-    timer = pg.QtCore.QTimer()
-    timer.timeout.connect(mw.plot_data)
-    timer.start(5)
     sys.exit(app.exec_())
 
 # app = pg.mkQApp()
